aa1fb3c7b4f4baf6b135d0caaea150580be35753
LuisJURU <luisjaraba@markdebrand.com>
2025-10-21 08:30:09 -0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="HSO Fuel Delivery - MVP", version="0.1.0", root_path=settin
 # CORS: permitir cualquier origen (para apps y web)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
